81835dcbd0001248fd2226696aba59669f02e843
Fredrik Fagerholm <fagerholm.fredrik@gmail.com>
2017-01-03 14:48:58 +0200

    Added representation for user, used when sending json response

--- mopidy_killthedj/user.py
+++ mopidy_killthedj/user.py
@@ -3,3 +3,6 @@ class User():
         self.username = username
         self.is_admin = is_admin
 
+    def __repr__(self):
+        return str('{"username": "%(username)s", "is_admin": %(is_admin)s}'
+                   % {"username": self.username, "is_admin": self.is_admin})
