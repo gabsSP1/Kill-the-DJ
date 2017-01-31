cec9758a0ba76042715813fd2b94bdf4f7b1f109
Fredrik Fagerholm <fagerholm.fredrik@gmail.com>
2017-01-31 09:43:31 +0200

    Added user votes

--- mopidy_killthedj/user.py
+++ mopidy_killthedj/user.py
@@ -1,8 +1,15 @@
 class User():
-    def __init__(self, username, cookie, is_admin=False):
+    def __init__(self, username, cookie, is_admin=False, votes_left=10):
         self.username = username
         self.cookie = cookie
         self.is_admin = is_admin
+        self.votes_left = votes_left
+
+    def decrement_votes(self):
+        if self.votes_left > 0:
+            self.votes_left -= 1
+        else:
+            raise Exception("user has no votes left")
 
     def __repr__(self):
         return str('{"username": "%(username)s", "is_admin": %(is_admin)s}'
